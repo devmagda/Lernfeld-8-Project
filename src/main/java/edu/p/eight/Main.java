0bef630303d4ef0f064d4f1cb9af553dc0e3b906
devmagda <113769198+devmagda@users.noreply.github.com>
2024-03-04 08:16:01 +0100

    Added new Tests
    A

--- src/main/java/edu/p/eight/Main.java
+++ src/main/java/edu/p/eight/Main.java
@@ -2,10 +2,7 @@ package edu.p.eight;
 
 class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println(StringUtils.add("Hello, ", null));
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        System.out.println(StringUtils.add("Hello, ", null));
+
     }
 }
---
+++ src/test/java/edu/p/eight/MainTest.java
@@ -0,0 +1,14 @@
+// StringUtilsTest.java
+package edu.p.eight;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testAdd() {
+
+    }
+}
