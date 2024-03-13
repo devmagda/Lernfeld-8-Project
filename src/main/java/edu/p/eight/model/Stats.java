659103234242b8b0da946809f68bbd498233a655
devmagda <113769198+devmagda@users.noreply.github.com>
2024-03-13 07:50:01 +0100

    Added new Stats and the possibility to start from project
    Also added import of config file to java.util.File

--- src/main/java/edu/p/eight/model/Stats.java
+++ src/main/java/edu/p/eight/model/Stats.java
@@ -4,15 +4,18 @@ public class Stats {
 
     public static final float DEFAULT_SPEED = 5f;
     public static final float DEFAULT_SPAWN_RATE = 0.25f;
-    public float speed;
-    public float spawnRate;
+    public static final float DEFAULT_INCREASE_RATE = 0.005f;
+    public static float speed;
+    public static float spawnRate;
+    public static float increaseRate;
 
-    public Stats(float speed, float spawnRate) {
+    public Stats(float speed, float spawnRate, float increaseRate) {
         this.speed = speed;
         this.spawnRate = spawnRate;
+        this.increaseRate = increaseRate;
     }
 
     public Stats() {
-        this(DEFAULT_SPEED, DEFAULT_SPAWN_RATE);
+        this(DEFAULT_SPEED, DEFAULT_SPAWN_RATE, DEFAULT_INCREASE_RATE);
     }
 }
